af106d874eeae9a828eaf83b6e90269d693ad698
Konrad Iwanczewski <konrad.iwanczewski@student.pk.edu.pl>
2025-05-27 13:34:47 +0200

    c

--- core/src/main/java/io/github/RotNG/Tree.java
+++ core/src/main/java/io/github/RotNG/Tree.java
@@ -8,15 +8,15 @@ import com.badlogic.gdx.math.Rectangle;
 public class Tree {
     private Texture texture;
     private Rectangle bounds;
-    private static final float SCALE = 0.5f;
+    private static final float SCALE = 0.2f;
 
     public Tree(float x, float y) {
-        texture = new Texture("tree.png");
-        bounds = new Rectangle(x, y, texture.getWidth(), texture.getHeight());
+        texture = new Texture("tree2.png");
+        bounds = new Rectangle(x, y, texture.getWidth() - 50, texture.getHeight() - 50);
     }
 
     public void render(SpriteBatch batch) {
-        batch.draw(texture, bounds.x, bounds.y);
+        batch.draw(texture, bounds.x - 50, bounds.y - 50);
     }
 
     public Rectangle getBounds() {
