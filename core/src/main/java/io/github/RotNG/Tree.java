b6d80aaa411d1156766347d85f21a05820bfa054
Konrad Iwanczewski <konrad.iwanczewski@student.pk.edu.pl>
2025-05-30 15:13:12 +0200

    c

--- core/src/main/java/io/github/RotNG/Tree.java
+++ core/src/main/java/io/github/RotNG/Tree.java
@@ -6,23 +6,29 @@ import com.badlogic.gdx.math.Rectangle;
 
 
 public class Tree {
+    //skladowe
     private Texture texture;
     private Rectangle bounds;
     private static final float SCALE = 0.2f;
 
+    //konstruktor
     public Tree(float x, float y) {
+        //tekstura i hitbox
         texture = new Texture("tree2.png");
         bounds = new Rectangle(x, y, texture.getWidth() - 50, texture.getHeight() - 50);
     }
 
+    //wyrenderowanie
     public void render(SpriteBatch batch) {
         batch.draw(texture, bounds.x - 50, bounds.y - 50);
     }
 
+    //getter
     public Rectangle getBounds() {
         return bounds;
     }
 
+    //usuwanie zasobów
     public void dispose() {
         texture.dispose();
     }
