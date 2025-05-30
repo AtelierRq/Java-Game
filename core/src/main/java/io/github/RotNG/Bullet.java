968f669f0dcc2ae1143e17972bae36e44bdb85ae
Konrad Iwanczewski <konrad.iwanczewski@student.pk.edu.pl>
2025-05-30 15:01:02 +0200

    c

--- core/src/main/java/io/github/RotNG/Bullet.java
+++ core/src/main/java/io/github/RotNG/Bullet.java
@@ -6,40 +6,52 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
 public class Bullet {
+    //ustawienie zasięgu i prędkosci pocisków
     private static final float SPEED = 500f;
     private static final float RANGE = 500f;
 
+    //składowe
     private Texture texture;
     private Rectangle bounds;
     private Vector2 position;
     private Vector2 velocity;
     private float distanceTraveled = 0;
 
+    //konstruktor
     public Bullet(Vector2 startPos, Vector2 targetPos) {
+
+        //ustawienie tekstury i pozycji
         texture = new Texture("bullet2.png");
-        position = new Vector2(startPos);
+        position = new Vector2(startPos); //ustawiienie pocisku tam gdzie jest gracz
+
+        //predkosc
         velocity = targetPos.sub(startPos).nor().scl(SPEED);
 
+        //hitboxy pociskow
         bounds = new Rectangle(position.x -50, position.y -50, texture.getWidth(), texture.getHeight());
     }
 
+    //aktualizacji pozycji pocisku, oraz sprawdzanie czy nie osiągnął maksymalnego zasięgu
     public boolean update(float deltaTime) {
         Vector2 movement = new Vector2(velocity).scl(deltaTime);
         position.add(movement);
         distanceTraveled += movement.len();
         bounds.setPosition(position);
 
-        return distanceTraveled < RANGE;
+        return distanceTraveled < RANGE; //Jeśli pocisk przeleciał mniej niż max RANGE, zwraca true, jesli nie - false
     }
 
+    //renderowanie grafiki pocisku
     public void render(SpriteBatch batch) {
         batch.draw(texture, position.x -50, position.y -50);
     }
 
+    //getter - udostepnia innym klasom prostokąt kolizji
     public Rectangle getBounds() {
         return bounds;
     }
 
+    //czyszczenie zasobów
     public void dispose() {
         texture.dispose();
     }
--- core/src/main/java/io/github/RotNG/Enemy.java
+++ core/src/main/java/io/github/RotNG/Enemy.java
@@ -8,6 +8,8 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
 public class Enemy {
+
+    //składowe
     private static final float SPEED = 30f;
     private static final int MAX_HP = 5;
 
@@ -18,6 +20,7 @@ public class Enemy {
     private float moveTimer = 0f;
     private Vector2 direction;
 
+    //tekstury przeciwników
     private static final String[] TEXTURE_PATHS = {
         "enemy4.png",
         "enemy5.png",
@@ -25,34 +28,42 @@ public class Enemy {
         "enemy7.png"
     };
 
+    //konstruktor
     public Enemy(float x, float y) {
 
         //ładowanie losowej tekstury do przeciwnikow (jednej z 4)
         String path = TEXTURE_PATHS[MathUtils.random(TEXTURE_PATHS.length - 1)];
         texture = new Texture(path);
 
+        //hitbox przeciwników
         bounds = new Rectangle(x, y, texture.getWidth(), texture.getHeight());
 
+        //losowe poruszanie się postaci
         hp = MAX_HP;
         direction = new Vector2(MathUtils.random(-1f, 1f), MathUtils.random(-1f, 1f)).nor();
     }
 
+    //odpowiada za losowy ruch przeciwnika co 0.5 sekundy
     public void update(float deltaTime) {
         moveTimer += deltaTime;
 
+        //ustalenie kierunku losowo
         if (moveTimer > 0.5f) {
             direction.set(MathUtils.random(-1f, 1f), MathUtils.random(-1f, 1f)).nor();
             moveTimer = 0f;
         }
 
+        //przesunięcie w ustalonym kierunku z ustaloną prędkością
         bounds.x += direction.x * SPEED * deltaTime;
         bounds.y += direction.y * SPEED * deltaTime;
     }
 
+    //wyrenderowanie przeciwnika
     public void render(SpriteBatch batch) {
         batch.draw(texture, bounds.x - 55, bounds.y - 55);
     }
 
+    //wyrenderowanie health bara
     public void renderHealthBar(ShapeRenderer shapeRenderer) {
         float barWidth = bounds.width;
         float barHeight = 5;
@@ -68,22 +79,27 @@ public class Enemy {
         //shapeRenderer.rect(bounds.x, bounds.y, bounds.width, bounds.height);
     }
 
+    //getter
     public Rectangle getBounds() {
         return bounds;
     }
 
+    //sparwdzanie czy hitboxy pocisków i przeciwników na siebie nachodzą
     public boolean overlaps(Rectangle bulletHitbox) {
         return bounds.overlaps(bulletHitbox);
     }
 
+    //sprawdzanie czy przeciwnik otrzymal obrazenia
     public void hit() {
         hp--;
     }
 
+    //sprawdzanie czy przeciwnik jest martwy
     public boolean isDead() {
         return hp <= 0;
     }
 
+    //czyszczenie zasobów
     public void dispose() {
         texture.dispose();
     }
